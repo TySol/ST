7ccd0eb1814c51382dacc49fe3fda707b5f8174a
Taylor <taylor_funk@live.com>
2014-09-19 12:20:18 -0600

    Heroku debug 2

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :storage => :s3, :bucket => 'stgutah', :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
