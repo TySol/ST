5afe90e6a51abc0df80690a7bf17785c2d4298ed
Taylor <taylor_funk@live.com>
2014-09-21 23:42:39 -0600

    Fix pins destroy

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:show, :edit, :update]
-  before_action :correct_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 1000)
